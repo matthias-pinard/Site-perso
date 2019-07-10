f15d19d3338975369c5ad0b2ec8388e6c6a2d81b
Matthias <matthiaspinard@gmail.com>
2019-07-10 22:50:37 +0200

    Icon project card + contact form

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -2,33 +2,31 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faYoutube} from '@fortawesome/free-brands-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import {faGlobe} from '@fortawesome/free-solid-svg-icons'
 import "../css/global.css"
 import { IProject } from "../interface/IProject"
 
 const ProjectCard = (projet: IProject) => {
   const background = projet.image
   return (
-    // <div className="max-w-sm shadow-lg rounded overflow-hidden">
-    //     <img className="w-full" src={Background}/>
-    //     <div className="px-6 py-4">
-    //         <div className="font-bold text-xl mb-2"> Titre</div>
-    //         <p className="text-gray-700 text-base">
-    //             lDeserunt esse ad irure anim nostrud esse officia consectetur elit ex est enim quis elit.
-    //         </p>
-    //     </div>
-    // </div>
-    <div className="max-w-md min-w-sm lg:max-w-full lg:flex flex-1 m-4 shadow-xl">
+    <div className="max-w-md min-w-sm lg:max-w-full lg:flex flex-1 m-4 shadow-xl self-stretch">
+      {projet.image &&
       <div
         className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden
         "
         style={{ backgroundImage: `url(${background})` }}
-      ></div>
+      ></div>}
       <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
         <div className="text-gray-600 font-bold text-xl mb-2">
           {projet.name}
         </div>
-        <p className="text-gray-700 text-base mb-4 text-justify">{projet.description}</p>
-        {/* <FontAwesomeIcon icon={faYoutube} size="lg"/> */}
+        <p className="text-gray-700 text-sm mb-4 text-justify">{projet.description}</p>
+        <div className="flex justify-around max-w-full">
+          {projet.video && <FontAwesomeIcon icon={faYoutube} size="lg"/>}
+          {projet.github && <FontAwesomeIcon icon={faGithub} size="lg"/>}
+          {projet.website && <FontAwesomeIcon icon={faGlobe} size="lg"/>}
+        </div>
       </div>
     </div>
   )
---
+++ src/pages/contact.tsx
@@ -0,0 +1,65 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default () => (
+  <Layout>
+    <SEO title="Contact" />
+    <div className="flex justify-center pt-6">
+      <form name="contact" className="w-full max-w-sm" netlify>
+        <div className="md:flex md:items-center  mb-6">
+          <div className="md:w-1/3">
+            <label htmlFor="mail" className="text-gray-500 font-bold">
+              Mail
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              type="mail"
+              id="mail"
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 w-full"
+            />
+          </div>
+        </div>
+
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label htmlFor="objet" className="text-gray-500 font-bold">
+              Objet
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              type="text"
+              id="objet"
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 w-full"
+            />
+          </div>
+        </div>
+
+        <div className="md:flex md:items-center mb-2">
+          <div className="md:w-1/3">
+            <label htmlFor="mail" className="text-gray-500 font-bold">
+              Message
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-3 w-full" />
+          </div>
+        </div>
+
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button
+              className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+            >Envoyer</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </Layout>
+)
