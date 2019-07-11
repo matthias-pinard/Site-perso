b5a8d137b71c6ca8cd37f7cbdc45576cb3e7f159
Matthias <matthiaspinard@gmail.com>
2019-07-11 22:00:58 +0200

    Index global

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,14 +1,11 @@
 import React from "react"
-import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ProjectCard from "../components/project-card"
-import { Query } from "../graphqlTypes"
 
 import ProjectView from "../components/pages/projects-views"
 
-export default (query: {data: Query}) => {
+export default () => {
   return (
   <Layout>
     <SEO title="Projects" />
