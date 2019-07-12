8527f8da29555bb5839ba641736a7cd8cbcadf0a
Matthias <matthiaspinard@gmail.com>
2019-07-12 18:26:56 +0200

    modif description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Matthias Pinard`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `A site about a robot.`,
     author: `@gatsbyjs`,
     navigation: [
       {
