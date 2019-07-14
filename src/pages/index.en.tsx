b3f39b1e8687251cfcac764987261e050f3ecf11
Matthias <matthiaspinard@gmail.com>
2019-07-14 16:44:07 +0200

    Revert "Test build error netlify"
    This reverts commit bc1bfbb6e8b1c3dbcc075a3900c7cb41575d1828.

---
+++ src/pages/index.en.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout/fr"
+import SEO from "../components/seo"
+import ContactForm from "../components/pages/contact-form"
+
+export default () => (
+  <Layout>
+    <SEO title="Contact" />
+    <div>
+        Test
+    </div>
+  </Layout>
+)
